c84127db8c7ea765f89df0ccb126d0743c64f30c
qikke <qikke1995@126.com>
2019-07-08 17:20:50 +0800

    add compiler.js

---
+++ bin/my-pack.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const path = require('path')
+
+const config = require(path.resolve('webpack.config.js'))
+
+const Compiler = require('../lib/Compiler.js')
+
+let compiler = new Compiler(config)
+
+compiler.run()
